4a2f98ac0da19da5abda1029fdd9ced90cf87eb9
Zack rahbar <zackrahbar@gwu.edu>
2026-02-02 14:34:29 -0500

    framework

---
+++ test0.java
@@ -0,0 +1,23 @@
+public class test0 {
+
+    private static void test1(){
+
+        //generate test data 
+        //generate expected output
+
+        //call the parser on the test data 
+
+        //read in the outputs 
+        //compare outputs to expected outputs
+
+        System.out.println("Test 1 Failed");
+
+    }
+    
+    public static void main(String[] args) {
+
+        test1();
+
+    }
+
+}
