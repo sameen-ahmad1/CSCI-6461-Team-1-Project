27bec03e8669aaf1c8e41cd4112f74112a2515d6
Zack rahbar <zackrahbar@gwu.edu>
2026-02-04 15:00:35 -0500

    sample data

--- test0.java
+++ test0.java
@@ -1,14 +1,50 @@
+import java.io.*;
+import java.util.*;
+
 public class test0 {
+
+    public static String getBuffer(String filePath){
+
+        StringBuilder buffer = new StringBuilder();
+        
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line);
+                buffer.append(System.lineSeparator()); // Add newline
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        String content = buffer.toString();
+
+        return content;
+
+    }
 
     private static void test1(){
 
         //generate test data 
+        String input = "./testFiles/test0_1/input.txt";
+
         //generate expected output
+        String expectedLoadFile = getBuffer("./testFiles/test0_1/expectedLoad.txt");
+        String expectedListingFile = getBuffer("./testFiles/test0_1/expectedListing.txt");
 
         //call the parser on the test data 
 
         //read in the outputs 
+        String loadFile = getBuffer("load.txt");
+        String listingFile = getBuffer("listing.txt");
+
         //compare outputs to expected outputs
+        if(loadFile.equals(expectedLoadFile) && listingFile.equals(expectedListingFile)){
+
+            System.out.println("Test 1 Passed");
+            return;
+
+        }
 
         System.out.println("Test 1 Failed");
 
