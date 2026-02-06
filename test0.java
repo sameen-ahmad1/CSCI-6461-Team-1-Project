2b314c06486627a0cba68adfc30cf8cc7e719eb6
Zack rahbar <zackrahbar@gwu.edu>
2026-02-06 13:39:36 -0500

    2nd test

--- test0.java
+++ test0.java
@@ -51,10 +51,40 @@ public class test0 {
         System.out.println("Test 1 Failed");
 
     }
+
+    private static void test2(){
+
+        //generate test data 
+        String input = "./testFiles/test0_1/test.asm";
+
+        //generate expected output
+        String expectedLoadFile = getBuffer("./testFiles/test0_2/expectedLoad.txt");
+        String expectedListingFile = getBuffer("./testFiles/test0_2/expectedListing.txt");
+
+        //call the parser on the test data 
+        Assembler.main(new String[]{input});
+
+        //read in the outputs 
+        // String loadFile = getBuffer("load.txt");
+        String listingFile = getBuffer("output.txt");
+
+        //compare outputs to expected outputs
+        // if(loadFile.equals(expectedLoadFile) && listingFile.equals(expectedListingFile)){
+        if(!listingFile.equals(expectedListingFile)){
+
+            System.out.println("Test 1 Passed");
+            return;
+
+        }
+
+        System.out.println("Test 1 Failed");
+
+    }
     
     public static void main(String[] args) {
 
         test1();
+        test2();
 
     }
 
