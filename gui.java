7915de860ac529ddeabc17f9b9562f34618cd73c
Zack rahbar <zackrahbar@gwu.edu>
2026-02-19 19:00:31 -0500

    fix sizing and add empty action listeners for buttons

--- gui.java
+++ gui.java
@@ -7,11 +7,11 @@ public class gui extends JFrame{
 
     public gui(){
 
-        JPanel outer = new JPanel(new BorderLayout());
+        JPanel outer = new JPanel(new BorderLayout(10,10));
 
-        JPanel firstNorth = new JPanel(new BorderLayout());
-        JPanel firstCenter = new JPanel(new BorderLayout());
-        JPanel firstEast = new JPanel(new BorderLayout());
+        JPanel firstNorth = new JPanel(new BorderLayout(10,10));
+        JPanel firstCenter = new JPanel(new BorderLayout(10,10));
+        JPanel firstEast = new JPanel(new BorderLayout(10,10));
 
         outer.add(firstNorth, BorderLayout.NORTH);
         outer.add(firstCenter, BorderLayout.CENTER);
@@ -23,37 +23,37 @@ public class gui extends JFrame{
         firstNorth.add(title, BorderLayout.NORTH);
         firstNorth.add(names, BorderLayout.SOUTH);
 
-        JPanel firstEastNorth = new JPanel(new BorderLayout());
-        JPanel firstEastCenter = new JPanel(new BorderLayout());
-        JPanel firstEastSouth = new JPanel(new BorderLayout());
+        JPanel firstEastNorth = new JPanel(new BorderLayout(5,5));
+        JPanel firstEastCenter = new JPanel(new BorderLayout(5,5));
+        JPanel firstEastSouth = new JPanel(new BorderLayout(5,5));
 
         firstEast.add(firstEastNorth, BorderLayout.NORTH);
         firstEast.add(firstEastCenter, BorderLayout.CENTER);
         firstEast.add(firstEastSouth, BorderLayout.SOUTH);
 
         JLabel cacheContentLabel = new JLabel("Cache Content");
-        JTextArea cacheContent = new JTextArea("");
+        JTextArea cacheContent = new JTextArea("",20,30);
         cacheContent.setEditable(false);
 
         firstEastNorth.add(cacheContentLabel, BorderLayout.NORTH);
         firstEastNorth.add(cacheContent, BorderLayout.SOUTH);
 
         JLabel printerLabel = new JLabel("Printer");
-        JTextArea printer = new JTextArea("");
+        JTextArea printer = new JTextArea("",10,20);
         printer.setEditable(false);
 
         firstEastCenter.add(printerLabel, BorderLayout.NORTH);
         firstEastCenter.add(printer, BorderLayout.SOUTH);
 
         JLabel consoleInputLabel = new JLabel("Console Input");
-        JTextField consoleInput = new JTextField("");
+        JTextField consoleInput = new JTextField("", 20);
         consoleInput.setEditable(false);
 
         firstEastSouth.add(consoleInputLabel, BorderLayout.NORTH);
         firstEastSouth.add(consoleInput, BorderLayout.SOUTH);
 
-        JPanel firstCenterNorth = new JPanel(new BorderLayout());
-        JPanel firstCenterCenter = new JPanel(new BorderLayout());
+        JPanel firstCenterNorth = new JPanel(new BorderLayout(5,5));
+        JPanel firstCenterCenter = new JPanel(new BorderLayout(5,5));
         JPanel firstCenterSouth = new JPanel(new FlowLayout());
 
         firstCenter.add(firstCenterNorth, BorderLayout.NORTH);
@@ -67,102 +67,134 @@ public class gui extends JFrame{
         firstCenterSouth.add(programFileLabel);
         firstCenterSouth.add(programFile);
 
-        JPanel firstCenterCenterWest = new JPanel(new BorderLayout());
-        JPanel firstCenterCenterCenter = new JPanel(new GridLayout(0, 1));
-        JPanel firstCenterCenterEast = new JPanel(new GridLayout(0, 1));
+        JPanel firstCenterCenterWest = new JPanel(new BorderLayout(5,5));
+        JPanel firstCenterCenterCenter = new JPanel(new GridLayout(0, 1, 5, 5));
+        JPanel firstCenterCenterEast = new JPanel(new GridLayout(0, 1, 5, 5));
 
         firstCenterCenter.add(firstCenterCenterWest, BorderLayout.WEST);
         firstCenterCenter.add(firstCenterCenterCenter, BorderLayout.CENTER);
         firstCenterCenter.add(firstCenterCenterEast, BorderLayout.EAST);
 
-        JPanel firstCenterCenterWestNorth = new JPanel(new BorderLayout());
-        JPanel firstCenterCenterWestSouth = new JPanel(new FlowLayout());
+        JPanel firstCenterCenterWestNorth = new JPanel(new BorderLayout(5,5));
+        JPanel firstCenterCenterWestSouth = new JPanel(new FlowLayout(FlowLayout.CENTER,5,5));
 
         firstCenterCenterWest.add(firstCenterCenterWestNorth, BorderLayout.NORTH);
         firstCenterCenterWest.add(firstCenterCenterWestSouth, BorderLayout.SOUTH);
 
         JLabel binaryLabel = new JLabel("Binary");
-        JTextField binary = new JTextField("");
+        JTextField binary = new JTextField("", 10);
         binary.setEditable(false);
 
         firstCenterCenterWestNorth.add(binaryLabel, BorderLayout.NORTH);
         firstCenterCenterWestNorth.add(binary, BorderLayout.SOUTH);
 
         JLabel octalInputLabel = new JLabel("Octal Input");
-        JTextField octalInput = new JTextField("");
+        JTextField octalInput = new JTextField("", 5);
 
         firstCenterCenterWestSouth.add(octalInputLabel);
         firstCenterCenterWestSouth.add(octalInput);
 
-        JPanel loadRow = new JPanel(new FlowLayout());
+        JPanel loadRow = new JPanel(new FlowLayout(FlowLayout.CENTER,5,5));
         JButton loadButton = new JButton();
         JLabel loadLabel = new JLabel("Load");
 
         loadRow.add(loadButton);
         loadRow.add(loadLabel);
 
-        JPanel loadPlusRow = new JPanel(new FlowLayout());
+        loadButton.addActionListener((e) -> {
+
+        });
+
+        JPanel loadPlusRow = new JPanel(new FlowLayout(FlowLayout.CENTER,5,5));
         JButton loadPlusButton = new JButton();
         JLabel loadPlusLabel = new JLabel("Load+");
 
         loadPlusRow.add(loadPlusButton);
         loadPlusRow.add(loadPlusLabel);
 
-        JPanel storeRow = new JPanel(new FlowLayout());
+        loadPlusButton.addActionListener((e) -> {
+
+        });
+
+        JPanel storeRow = new JPanel(new FlowLayout(FlowLayout.CENTER,5,5));
         JButton storeButton = new JButton();
         JLabel storeLabel = new JLabel("Store");
 
         storeRow.add(storeButton);
         storeRow.add(storeLabel);
 
-        JPanel storePlusRow = new JPanel(new FlowLayout());
-        JButton storelusButton = new JButton();
+        storeButton.addActionListener((e) -> {
+
+        });
+
+        JPanel storePlusRow = new JPanel(new FlowLayout(FlowLayout.CENTER,5,5));
+        JButton storePlusButton = new JButton();
         JLabel storePlusLabel = new JLabel("Store+");
 
-        storePlusRow.add(storelusButton);
+        storePlusRow.add(storePlusButton);
         storePlusRow.add(storePlusLabel);
+
+        storePlusButton.addActionListener((e) -> {
+
+        });
 
         firstCenterCenterCenter.add(loadRow);
         firstCenterCenterCenter.add(loadPlusRow);
         firstCenterCenterCenter.add(storeRow);
         firstCenterCenterCenter.add(storePlusRow);
 
-        JPanel runRow = new JPanel(new FlowLayout());
+        JPanel runRow = new JPanel(new FlowLayout(FlowLayout.CENTER,5,5));
         JButton runButton = new JButton();
         JLabel runLabel = new JLabel("Run");
 
         runRow.add(runButton);
         runRow.add(runLabel);
 
-        JPanel stepRow = new JPanel(new FlowLayout());
+        runButton.addActionListener((e) -> {
+
+        });
+
+        JPanel stepRow = new JPanel(new FlowLayout(FlowLayout.CENTER,5,5));
         JButton stepButton = new JButton();
         JLabel stepLabel = new JLabel("Step");
 
         stepRow.add(stepButton);
         stepRow.add(stepLabel);
 
-        JPanel haltRow = new JPanel(new FlowLayout());
+        stepButton.addActionListener((e) -> {
+
+        });
+
+        JPanel haltRow = new JPanel(new FlowLayout(FlowLayout.CENTER,5,5));
         JButton haltButton = new JButton();
         JLabel haltLabel = new JLabel("Halt");
 
         haltRow.add(haltButton);
         haltRow.add(haltLabel);
 
-        JPanel IPLRow = new JPanel(new FlowLayout());
+        haltButton.addActionListener((e) -> {
+
+        });
+
+        JPanel IPLRow = new JPanel(new FlowLayout(FlowLayout.CENTER,5,5));
         JButton IPLButton = new JButton();
         JLabel IPLLabel = new JLabel("IPL");
 
         IPLRow.add(IPLButton);
         IPLRow.add(IPLLabel);
 
+        IPLButton.addActionListener((e) -> {
+
+        });
+
         firstCenterCenterEast.add(runRow);
         firstCenterCenterEast.add(stepRow);
         firstCenterCenterEast.add(haltRow);
         firstCenterCenterEast.add(IPLRow);
 
-        JPanel firstCenterNorthWest = new JPanel(new GridLayout(0, 1));
-        JPanel firstCenterNorthCenter = new JPanel(new GridLayout(0, 1));
-        JPanel firstCenterNorthEast = new JPanel(new GridLayout(0, 1));
+        JPanel firstCenterNorthWest = new JPanel(new GridLayout(0, 1, 5, 5));
+        JPanel firstCenterNorthCenter = new JPanel(new GridLayout(0, 1, 5, 5));
+        JPanel firstCenterNorthEast = new JPanel(new GridLayout(0, 1, 5, 5));
 
         firstCenterNorth.add(firstCenterNorthWest, BorderLayout.WEST);
         firstCenterNorth.add(firstCenterNorthCenter, BorderLayout.CENTER);
@@ -170,41 +202,57 @@ public class gui extends JFrame{
 
         JLabel gprLabel = new JLabel("GPR");
 
-        JPanel zeroRowGPR = new JPanel(new FlowLayout());
+        JPanel zeroRowGPR = new JPanel(new FlowLayout(FlowLayout.CENTER,5,5));
         JLabel zeroLabelGPR = new JLabel("0");
-        JTextField zeroTextGPR = new JTextField("");
+        JTextField zeroTextGPR = new JTextField("",10);
         JButton zeroButtonGPR = new JButton();
 
         zeroRowGPR.add(zeroLabelGPR);
         zeroRowGPR.add(zeroTextGPR);
         zeroRowGPR.add(zeroButtonGPR);
 
-        JPanel oneRowGPR = new JPanel(new FlowLayout());
+        zeroButtonGPR.addActionListener((e) -> {
+
+        });
+
+        JPanel oneRowGPR = new JPanel(new FlowLayout(FlowLayout.CENTER,5,5));
         JLabel oneLabelGPR = new JLabel("1");
-        JTextField oneTextGPR = new JTextField("");
+        JTextField oneTextGPR = new JTextField("",10);
         JButton oneButtonGPR = new JButton();
 
         oneRowGPR.add(oneLabelGPR);
         oneRowGPR.add(oneTextGPR);
         oneRowGPR.add(oneButtonGPR);
 
-        JPanel twoRowGPR = new JPanel(new FlowLayout());
+        oneButtonGPR.addActionListener((e) -> {
+
+        });
+
+        JPanel twoRowGPR = new JPanel(new FlowLayout(FlowLayout.CENTER,5,5));
         JLabel twoLabelGPR = new JLabel("2");
-        JTextField twoTextGPR = new JTextField("");
+        JTextField twoTextGPR = new JTextField("",10);
         JButton twoButtonGPR = new JButton();
 
         twoRowGPR.add(twoLabelGPR);
         twoRowGPR.add(twoTextGPR);
         twoRowGPR.add(twoButtonGPR);
 
-        JPanel threeRowGPR = new JPanel(new FlowLayout());
+        twoButtonGPR.addActionListener((e) -> {
+
+        });
+
+        JPanel threeRowGPR = new JPanel(new FlowLayout(FlowLayout.CENTER,5,5));
         JLabel threeLabelGPR = new JLabel("3");
-        JTextField threeTextGPR = new JTextField("");
+        JTextField threeTextGPR = new JTextField("",10);
         JButton threeButtonGPR = new JButton();
 
         threeRowGPR.add(threeLabelGPR);
         threeRowGPR.add(threeTextGPR);
         threeRowGPR.add(threeButtonGPR);
+
+        threeButtonGPR.addActionListener((e) -> {
+
+        });
 
         firstCenterNorthWest.add(gprLabel);
         firstCenterNorthWest.add(zeroRowGPR);
@@ -214,34 +262,46 @@ public class gui extends JFrame{
 
         JLabel ixrLabel = new JLabel("IXR");
 
-        JPanel zeroRowIXR = new JPanel(new FlowLayout());
+        JPanel zeroRowIXR = new JPanel(new FlowLayout(FlowLayout.CENTER,5,5));
 
-        JPanel oneRowIXR = new JPanel(new FlowLayout());
+        JPanel oneRowIXR = new JPanel(new FlowLayout(FlowLayout.CENTER,5,5));
         JLabel oneLabelIXR = new JLabel("1");
-        JTextField oneTextIXR = new JTextField("");
+        JTextField oneTextIXR = new JTextField("",10);
         JButton oneButtonIXR = new JButton();
 
         oneRowIXR.add(oneLabelIXR);
         oneRowIXR.add(oneTextIXR);
         oneRowIXR.add(oneButtonIXR);
 
-        JPanel twoRowIXR = new JPanel(new FlowLayout());
+        oneButtonIXR.addActionListener((e) -> {
+
+        });
+
+        JPanel twoRowIXR = new JPanel(new FlowLayout(FlowLayout.CENTER,5,5));
         JLabel twoLabelIXR = new JLabel("2");
-        JTextField twoTextIXR= new JTextField("");
+        JTextField twoTextIXR= new JTextField("",10);
         JButton twoButtonIXR = new JButton();
 
         twoRowIXR.add(twoLabelIXR);
         twoRowIXR.add(twoTextIXR);
         twoRowIXR.add(twoButtonIXR);
 
-        JPanel threeRowIXR = new JPanel(new FlowLayout());
+        twoButtonIXR.addActionListener((e) -> {
+
+        });
+
+        JPanel threeRowIXR = new JPanel(new FlowLayout(FlowLayout.CENTER,5,5));
         JLabel threeLabelIXR = new JLabel("3");
-        JTextField threeTextIXR = new JTextField("");
+        JTextField threeTextIXR = new JTextField("",10);
         JButton threeButtonIXR = new JButton();
 
         threeRowIXR.add(threeLabelIXR);
         threeRowIXR.add(threeTextIXR);
         threeRowIXR.add(threeButtonIXR);
+
+        threeButtonIXR.addActionListener((e) -> {
+
+        });
 
         firstCenterNorthCenter.add(ixrLabel);
         firstCenterNorthCenter.add(zeroRowIXR);
@@ -249,58 +309,74 @@ public class gui extends JFrame{
         firstCenterNorthCenter.add(twoRowIXR);
         firstCenterNorthCenter.add(threeRowIXR);
 
-        JPanel pcRow = new JPanel(new FlowLayout());
+        JPanel pcRow = new JPanel(new FlowLayout(FlowLayout.CENTER,5,5));
         JLabel pcLabel = new JLabel("PC");
-        JTextField pcText = new JTextField("");
+        JTextField pcText = new JTextField("",10);
         JButton pcButton = new JButton();
 
         pcRow.add(pcLabel);
         pcRow.add(pcText);
         pcRow.add(pcButton);
 
-        JPanel marRow = new JPanel(new FlowLayout());
+        pcButton.addActionListener((e) -> {
+
+        });
+
+        JPanel marRow = new JPanel(new FlowLayout(FlowLayout.CENTER,5,5));
         JLabel marLabel = new JLabel("MAR");
-        JTextField marText = new JTextField("");
+        JTextField marText = new JTextField("",10);
         JButton marButton = new JButton();
 
         marRow.add(marLabel);
         marRow.add(marText);
         marRow.add(marButton);
 
-        JPanel mbrRow = new JPanel(new FlowLayout());
+        marButton.addActionListener((e) -> {
+
+        });
+
+        JPanel mbrRow = new JPanel(new FlowLayout(FlowLayout.CENTER,5,5));
         JLabel mbrLabel = new JLabel("MBR");
-        JTextField mbrText = new JTextField("");
+        JTextField mbrText = new JTextField("",10);
         JButton mbrButton = new JButton();
 
         mbrRow.add(mbrLabel);
         mbrRow.add(mbrText);
         mbrRow.add(mbrButton);
 
-        JPanel irRow = new JPanel(new FlowLayout());
+        mbrButton.addActionListener((e) -> {
+
+        });
+
+        JPanel irRow = new JPanel(new FlowLayout(FlowLayout.CENTER,5,5));
         JLabel irLabel = new JLabel("IR");
-        JTextField irText = new JTextField("");
+        JTextField irText = new JTextField("",10);
         JButton irButton = new JButton();
 
         irRow.add(irLabel);
         irRow.add(irText);
         irRow.add(irButton);
 
-        JPanel ccRow = new JPanel(new FlowLayout());
+        irButton.addActionListener((e) -> {
+
+        });
+
+        JPanel ccRow = new JPanel(new FlowLayout(FlowLayout.CENTER,5,5));
         JLabel ccLabel = new JLabel("CC");
-        JPanel ccTexts = new JPanel(new BorderLayout());
+        JPanel ccTexts = new JPanel(new BorderLayout(5,5));
         JLabel oudeText = new JLabel("OUDE");
-        JTextField ccText = new JTextField("");
+        JTextField ccText = new JTextField("",5);
 
         ccTexts.add(ccText, BorderLayout.NORTH);
         ccTexts.add(oudeText, BorderLayout.SOUTH);
         ccRow.add(ccLabel);
         ccRow.add(ccTexts);
 
-        JPanel mfrRow = new JPanel(new FlowLayout());
+        JPanel mfrRow = new JPanel(new FlowLayout(FlowLayout.CENTER,5,5));
         JLabel mfrLabel = new JLabel("MFR");
-        JPanel mfrTexts = new JPanel(new BorderLayout());
+        JPanel mfrTexts = new JPanel(new BorderLayout(5,5));
         JLabel mortText = new JLabel("MOTR");
-        JTextField mfrText = new JTextField("");
+        JTextField mfrText = new JTextField("",5);
 
         mfrTexts.add(mfrText, BorderLayout.NORTH);
         mfrTexts.add(mortText, BorderLayout.SOUTH);
